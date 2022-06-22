455aac883e538e558e173d82b1f7b446b631129d
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-06-22 11:59:18 +0200

    Add files via upload

---
+++ dasit_network_new.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#Script configurazione interfacce di RETE per Proxmox PVE DASIT - Federico Dalè
+#Config Porte ETH 1 e 3 in failover per la rete OSPEDALIERA in DHCP, Porte ETH 2 e 4 failover rete DASIT ip statico 192.168.2.19
+echo "Script Configurazione Network PVE DASIT - Federico Dale"
+echo ""
+echo "--- Configurazione Rete OSPEDALIERA ---"
+echo ""
+echo "     ETH 1/3 failover  "
+echo "     IP: DHCP"
+echo ""
+echo "---  End Config ---"
+echo ""
+echo "--- Configurazione Rete DASIT ---"
+echo ""
+echo "     ETH 2/4 failover  "
+echo "     IP: 192.168.2.19"
+echo ""
+echo "---  End Config ---"
+curl -OL https://raw.githubusercontent.com/fededale95/DASITScript/main/interfaces
+mv interfaces interfaces_test
+mv interfaces_test /etc/network
+#ifup -a
