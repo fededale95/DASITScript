e7b2533aa8af0303641a59066c46d2322afac9f7
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-06-23 15:54:01 +0200

    Update test_sshpwshell.sh

--- test_sshpwshell.sh
+++ test_sshpwshell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl -OL https://raw.githubusercontent.com/fededale95/DASITScript/main/interfaces
 cp /root/test.sh /root/test_copia.sh
-hostname -I | awk '{print $2}
+hostname -I | awk '{print $2}'
