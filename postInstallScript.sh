d542ea62b52597801fa865d6915b0420c6d9da85
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-06-27 10:34:43 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash -ex
 set -euo pipefail
 shopt -s inherit_errexit nullglob
-YW=`echo "\033[33m"`
-BL=`echo "\033[36m"`
-RD=`echo "\033[01;31m"`
-BGN=`echo "\033[4;92m"`
-GN=`echo "\033[1;92m"`
-DGN=`echo "\033[32m"`
-CL=`echo "\033[m"`
-BFR="\\r\\033[K"
-HOLD="-"
-CM="${GN}✓${CL}"
-CROSS="${RD}✗${CL}"
 
 if [ `pveversion | grep "pve-manager/7" | wc -l` -ne 1 ]; then
         echo -e "This script requires Proxmox Virtual Environment 7.0 or greater"
