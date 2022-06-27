ad47d3dd9402f58b0d9c5b77b8eeeb650764b8cb
fededale95 <32536729+fededale95@users.noreply.github.com>
2022-06-27 10:31:42 +0200

    Update postInstallScript.sh

--- postInstallScript.sh
+++ postInstallScript.sh
@@ -20,22 +20,11 @@ if [ `pveversion | grep "pve-manager/7" | wc -l` -ne 1 ]; then
         exit
 fi
 
-
-function msg_info() {
-    local msg="$1"
-    echo -ne " ${HOLD} ${YW}${msg}..."
-}
-
-function msg_ok() {
-    local msg="$1"
-    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
-}
-
 clear
 
 sleep 2
 sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
-msg_ok "Disabled Enterprise Repository"
+echo "Disabled Enterprise Repository"
 
 cat <<EOF > /etc/apt/sources.list
 deb http://ftp.debian.org/debian bullseye main contrib
@@ -43,27 +32,27 @@ deb http://ftp.debian.org/debian bullseye-updates main contrib
 deb http://security.debian.org/debian-security bullseye-security main contrib
 EOF
 sleep 2
-msg_ok "Added or Corrected PVE7 Sources"
+echo "Added or Corrected PVE7 Sources"
 
 cat <<EOF >> /etc/apt/sources.list
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 EOF
 sleep 2
-msg_ok "Enabled No-Subscription Repository"
+echo "Enabled No-Subscription Repository"
 
 cat <<EOF >> /etc/apt/sources.list
 # deb http://download.proxmox.com/debian/pve bullseye pvetest
 EOF
 sleep 2
-msg_ok "Added Beta/Test Repository"
+echo "Added Beta/Test Repository"
 
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
 apt --reinstall install proxmox-widget-toolkit &>/dev/null
-msg_ok "Disabled Subscription Nag"
+echo "Disabled Subscription Nag"
 
 apt-get update &>/dev/null
 apt-get -y dist-upgrade &>/dev/null
-msg_ok "Updated Proxmox VE 7"
+echo "Updated Proxmox VE 7"
 
 sleep 2
-msg_ok "Finished Post Install Routines"
+echo "Finished Post Install Routines"
